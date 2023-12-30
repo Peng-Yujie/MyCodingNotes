a6ad256377e45129288fddea3d0ffe71616b1ea9
YJ_ <Pumbaaxx@icloud.com>
2023-12-29 21:59:56 -0800

    coding cha 1

--- JavaScript/Asynchronous/script.js
+++ JavaScript/Asynchronous/script.js
@@ -56,9 +56,9 @@ getCountryAndNeighbour('Korea (Republic of)');
 ///////////////////////////////////////
 // Promise
 
-const renderCountry = function (data) {
+const renderCountry = function (data, className = '') {
   const html = `
-    <article class="country">
+    <article class="country ${className}">
       <img class="country__img" src="${data.flag}" />
       <div class="country__data">
         <h3 class="country__name">${data.name}</h3>
@@ -70,12 +70,90 @@ const renderCountry = function (data) {
     </article>
   `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+  // countriesContainer.style.opacity = 1;
 }
 
-const getCountryData = function (country) {
-  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`) // fetch return promise
-    .then((response) => response.json()) // return promise
-    .then(data => renderCountry(data[0])); // render data
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
 }
-getCountryData('canada');
+
+const getJSON = function (url, errorMsg = 'Something went wrong!') {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+      return response.json();
+    });
+}
+
+// const getCountryData = function (country) {
+//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`) // fetch return promise
+//     .then(response => {
+//       console.log(response);
+//       if (!response.ok) {
+//         throw new Error(`Country not found (${response.status})`);
+//       }
+//       return response.json();
+//     }) // return promise
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
+//       if (!neighbour) return;
+//       return fetch(`https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`); // return a new promise
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.log(`${err}`);
+//       renderError(`Something went wrong 🥲 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// }
+
+const getCountryData = function (country) {
+  getJSON(`https://countries-api-836d.onrender.com/countries/name/${country}`, 'Country not found')
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      if (!neighbour) throw new Error('No neighbour found!');
+      // Country 2
+      return getJSON(`https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`, 'Country not found');
+    })
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.log(`${err}`);
+      renderError(`Something went wrong 🥲 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+}
+
+btn.addEventListener('click', function () {
+  getCountryData('japan');
+});
+
+
+///////////////////////////////////////
+// Coding challenge #1
+/*
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => {
+      if (!response.ok) throw new Error(`Something went wrong (${response.status})`);
+      return response.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.city}, ${data.country}`);
+      getCountryData(`${data.country}`);
+    })
+    .catch(err => console.log(`${err}`));
+}
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+*/

a81652e702128a0bf2f68cbf6cb1f0e4a5e60033
YJ_ <Pumbaaxx@icloud.com>
2024-01-09 20:07:34 -0800

    props api

--- React/usepopcorn/src/index.js
+++ React/usepopcorn/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
@@ -8,6 +8,25 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <Mod />
   </React.StrictMode>
 );
+
+function Mod() {
+  const [starRating, setStatRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        size={24}
+        color="red"
+        maxRating={10}
+        onSetState={setStatRating}
+      />
+      <p>You rate {starRating} stars</p>
+    </div>
+  );
+}

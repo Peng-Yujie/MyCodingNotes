28bdf00f5d20397774d1964ad2d5f1ec67115122
YJ_ <Pumbaaxx@icloud.com>
2024-05-02 11:02:06 -0700

    handle checkout

--- React/the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ React/the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 import BookingDataBox from "./BookingDataBox";
 import Row from "../../ui/Row";
@@ -7,11 +8,11 @@ import Tag from "../../ui/Tag";
 import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
+import Spinner from "../../ui/Spinner";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "../../hooks/useBooking";
-import Spinner from "../../ui/Spinner";
-import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../../hooks/useCheckout";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -21,6 +22,7 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
+  const { checkout, isCheckingOut } = useCheckout();
   const moveBack = useMoveBack();
   const navigate = useNavigate();
 
@@ -50,6 +52,11 @@ function BookingDetail() {
         {status === "unconfirmed" && (
           <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
             Check in
+          </Button>
+        )}
+        {status === "checked-in" && (
+          <Button onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
+            Check out
           </Button>
         )}
         <Button $variation="secondary" onClick={moveBack}>

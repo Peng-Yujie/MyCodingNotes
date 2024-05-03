2e41124219893fe93fe147f4135e27216d6b00df
YJ_ <Pumbaaxx@icloud.com>
2024-05-02 17:19:28 -0700

    dark mode

--- React/the-wild-oasis/src/hooks/useUpdateUser.js
+++ React/the-wild-oasis/src/hooks/useUpdateUser.js
@@ -7,9 +7,9 @@ export function useUpdateUser() {
 
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
+    onSuccess: ({ user }) => {
       toast.success("User data updated successfully");
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.setQueryData(["user"], user);
     },
     onError: (err) => toast.error(err.message),
   });

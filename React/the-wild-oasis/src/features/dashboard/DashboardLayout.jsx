ccbd6b37e7329064ee8e4a126a03107fcb7476ce
YJ_ <Pumbaaxx@icloud.com>
2024-05-02 20:21:19 -0700

    dashboard

--- React/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ React/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import Stats from "./Stats";
+import Spinner from "../../ui/Spinner";
+import { useRecentBookings } from "../../hooks/useRecentBookings";
+import { useRecentStays } from "../../hooks/useRecentStays";
+import { useCabins } from "../../hooks/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +11,32 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading, bookings } = useRecentBookings();
+  const {
+    isLoading: isLoadingStays,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoadingCabins } = useCabins();
+
+  if (isLoading || isLoadingStays || isLoadingCabins) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today's activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
